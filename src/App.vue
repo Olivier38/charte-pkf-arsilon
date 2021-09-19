2bf6d46594aaf2b723666f7e379bc82081d4d3c6
Olivier TRAVEAUX <olivier.traveaux@arsilon.com>
2021-09-19 17:07:57 +0200

    Set of colors

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+    <section class="container">
+      <color v-for="color in colors"
+      :key="color.name"
+      :color="color"
+      />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Color from './components/Color';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Color
+  },
+  data() {
+    return {
+      colors: [
+        {name: 'bleu', cmjn: 'C100 - M75 - J0 - N6', rvb: 'R0 - V53 - B148', hexa: '#003594', pantone: '661 C'},
+        {name: 'noir', cmjn: 'N100', rvb: 'R0 - V0 - B0', hexa: '#000000', pantone: 'PANTONE BLACK'},
+        {name: 'gris', cmjn: 'C20 - M14 - J12 - N40', rvb: 'R151 - V153 - B155', hexa: '#97999B', pantone: 'COOL GREY 7'},
+        {name: 'light-blue', cmjn: 'C75 - M0 - J5 - N40', rvb: 'R0 - V142 - B32', hexa: '#00B5E2', pantone: 'Pantone 306'},
+        {name: 'mid-blue', cmjn: 'C100 - M0 - J20 - N0', rvb: 'R0 - V142 - B170', hexa: '#008EAA', pantone: 'Pantone 3135'},
+        {name: 'light-green', cmjn: 'C65 - M0 - J100 - N0', rvb: 'R120 - V190 - B32', hexa: '#78BE20', pantone: 'Pantone 368'},
+        {name: 'dark-green', cmjn: 'C47 - M11 - J99 - N64', rvb: 'R89 - V98 - B29', hexa: '#59621D', pantone: 'Pantone 378'},
+        {name: 'yellow', cmjn: 'C0 - M19 - J99 - 64', rvb: 'R255 - V199 - B44', hexa: '#59621D', pantone: 'Pantone 123'},
+        {name: 'lime', cmjn: 'C18 - M0 - J82 - N0', rvb: 'R210 - V228 - B66', hexa: '#DBE442', pantone: 'Pantone 360'},
+        {name: 'orange', cmjn: 'C0 - M65 - J100 - N0', rvb: 'R254 - V80 - B0', hexa: '#FE5000', pantone: 'Pantone 021'},
+        {name: 'red', cmjn: 'C0 - M96 - J93 - N2', rvb: 'R210 - V38 - B48', hexa: '#D22630', pantone: 'Pantone 1795'},
+        {name: 'pink', cmjn: 'C20 - M89 - J0 - N0', rvb: 'R197 - V41 - B155', hexa: '#C5299B', pantone: 'Pantone 240'},
+        {name: 'purple', cmjn: 'C66 - M100 - J8 - N27', rvb: 'R100 - V38 - B103', hexa: '#C5299B', pantone: 'Pantone 260'}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.bleu {
+  background-color: #003594
+}
+.noir {
+  background-color: #000000;
+}
+.gris {
+  background-color: #97999B;
+}
+.light-blue {
+  background-color: #00B5E2;
+}
+.mid-blue {
+  background-color: #008EAA;
+}
+.light-green {
+  background-color: #78BE20;
+}
+.dark-green {
+  background-color: #59621D;
+}
+.yellow {
+  background-color: #FFC72C;
+}
+.lime {
+  background-color: #DBE442;
+}
+.orange {
+  background-color: #FE5000;
+}
+.red {
+  background-color: #D22630;
+}
+.pink {
+  background-color: #C5299B;
+}
+.purple {
+  background-color: #642667;
+}
+
+
+.container {
+  margin: 0 auto;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+}
+.color {
+  width: 200px;
+  height: 33vh;
+}
+.content {
+  color: #FFFFFF;
+  margin: 15px;
+  font-family: arial;
+  font-size: 12px;
+  position: relative;
+  bottom: 1àpx;
+
 }
 </style>
---
+++ src/components/Color.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="color.name" class="color">
+        <div class="content">
+            <div>{{color.name}}</div>
+            <div>{{color.cmjn}}</div>
+            <div>{{color.rvb}}</div>
+            <div>{{color.hexa}}</div>
+            <div>{{color.pantone}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+        name: 'color',
+        props: ['color']
+}
+</script>
